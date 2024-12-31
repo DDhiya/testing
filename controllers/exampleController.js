6564e749b0b9ea332eecb40ff1681cc2c9958e3d
DDhiya <dhiyadanial@gmail.com>
2024-12-31 15:00:51 +0800

    add route hellofs

--- controllers/exampleController.js
+++ controllers/exampleController.js
@@ -1,3 +1,7 @@
 exports.getExample = (req, res) => {
     res.json({ message: 'Hello, World!' });
 };
+
+exports.helloFs = (req, res) => {
+    res.json({ message: "Hello, FS" });
+};
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const exampleController = require('../controllers/exampleController');
+const { helloFs } = require('../controllers/exampleController');
 
 router.get('/example', exampleController.getExample);
+router.get('/fs', helloFs);
 
 module.exports = router;
